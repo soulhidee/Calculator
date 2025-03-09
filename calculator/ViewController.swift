9fdedcc3eb52bc4ed70ff23253d15976d6131d6e
Daniil Isakov <serverpgig@yandex.ru>
2025-03-09 21:25:08 +0300

    Start

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Даниил on 09.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
