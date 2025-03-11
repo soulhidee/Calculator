8872a5d6600dd2e682a48e665a6ed32776db44c4
Daniil Isakov <serverpgig@yandex.ru>
2025-03-11 19:30:31 +0300

    Separate equals and clear button actions

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -12,17 +12,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ViewController loaded")
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func numberPressed(_ sender: UIButton) {
+        let number = sender.tag
+        let numberString = String(number)
+        displayLabel.text = displayLabel.text == "0" ? numberString : (displayLabel.text ?? "") + numberString
     }
     
     @IBAction func operationPressed(_ sender: UIButton) {
+        switch sender.tag {
+            case 10:
+                print("Pressed +")
+            case 11:
+                print("Pressed -")
+            case 12:
+                print("Pressed ×")
+            case 13:
+                print("Pressed ÷")
+            default:
+                print("Unknown operation tag: \(sender.tag)")
+            }
     }
     
     @IBAction func equalsPressed(_ sender: UIButton) {
+        print("Pressed =")
     }
     @IBAction func clearPressed(_ sender: UIButton) {
+        displayLabel.text = "0"
     }
     
     
