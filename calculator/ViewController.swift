758e8a836c5ea44059c89a2da8d458c86dd4a308
Daniil Isakov <serverpgig@yandex.ru>
2025-03-11 22:37:50 +0300

    Implement basic calculator operations

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
+    private var firstNumber: Double = 0
+    private var currentOperation: String?
+    private var secondNumber: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,25 +26,48 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operationPressed(_ sender: UIButton) {
-        switch sender.tag {
-            case 10:
-                print("Pressed +")
-            case 11:
-                print("Pressed -")
-            case 12:
-                print("Pressed ×")
-            case 13:
-                print("Pressed ÷")
-            default:
-                print("Unknown operation tag: \(sender.tag)")
+        if let text = displayLabel.text, let number = Double(text) {
+            firstNumber = number
+        }
+        currentOperation = {
+            switch sender.tag {
+            case 10: return "+"
+            case 11: return "-"
+            case 12: return "*"
+            case 13: return "÷"
+            default: return nil
             }
+        }()
+        displayLabel.text = "0"
     }
     
     @IBAction func equalsPressed(_ sender: UIButton) {
-        print("Pressed =")
-    }
+         if let text = displayLabel.text, let number = Double(text) {
+             secondNumber = number
+         }
+        guard let operation = currentOperation else { return }
+        var result: Double = 0
+        switch operation {
+        case "+":
+            result = firstNumber + secondNumber
+        case "-":
+            result = firstNumber - secondNumber
+        case "*":
+            result = firstNumber * secondNumber
+        case "÷":
+            result = firstNumber / secondNumber
+        default:
+            break
+        }
+        
+        displayLabel.text = String(result)
+     }
+    
     @IBAction func clearPressed(_ sender: UIButton) {
         displayLabel.text = "0"
+        firstNumber = 0
+        secondNumber = 0
+        currentOperation = nil
     }
     
     
