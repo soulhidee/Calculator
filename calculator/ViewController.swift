cd4209a9ad87d1e29b81d26d0edeb516e9bd49a2
Daniil Isakov <serverpgig@yandex.ru>
2025-03-11 18:25:49 +0300

    Set up IBOutlets and IBActions for calculator buttons

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func numberPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func equalsPressed(_ sender: UIButton) {
+    }
+    @IBAction func clearPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
 }
 
